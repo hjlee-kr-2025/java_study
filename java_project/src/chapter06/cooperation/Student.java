a7f2f7a4da620cb8a662c169a33d8c7968092be3
hjlee-kr-2025 <hjlee-kr@naver.com>
2025-02-07 17:50:37 +0900

    250207-8교시F

--- java_project/src/chapter06/cooperation/Student.java
+++ java_project/src/chapter06/cooperation/Student.java
@@ -11,6 +11,16 @@ public class Student {
 		this.money = money;
 	}
 	
+	public void takeBus(Bus bus) {
+		bus.take(1000);		// 버스 탑승비용은 1000원
+		this.money -= 1000;	// 버스 탑승비용 제외
+	}
+	
+	public void takeSubway(Subway subway) {
+		subway.take(1500);	// 지하철 탑승비용은 1500원
+		this.money -= 1500;	// 지하철 탑승비용 제외
+	}
+	
 	public void showInfo() {
 		System.out.println(studentName + "님의 남은 돈은 "
 			+ money + "원 입니다.");
