1ae1dd78abdd55d418dc0c337e6466daf6592c4b
hjlee-kr-2025 <hjlee-kr@naver.com>
2025-02-21 17:48:23 +0900

    250221-8교시

--- java_project/src/chapter13/stream/UserInfoTest1.java
+++ java_project/src/chapter13/stream/UserInfoTest1.java
@@ -10,7 +10,7 @@ public class UserInfoTest1 {
 		UserInfo userLee = new UserInfo("이철수", 40);
 		UserInfo userSong = new UserInfo("송영수", 55);
 		UserInfo userMoon = new UserInfo("문상원", 25);
-		UserInfo userShin = new UserInfo("신기원", 35);
+		UserInfo userShin = new UserInfo("김상원", 35);
 		
 		List<UserInfo> userInfoList = new ArrayList<UserInfo>();
 		userInfoList.add(userKim);
@@ -22,7 +22,8 @@ public class UserInfoTest1 {
 		
 		// 스트림사용
 		userInfoList.stream()
-			.filter(user -> user.getAge() >= 35)	// 조건에 따라서 데이터를 걸르는것
+			//.filter(user -> user.getAge() >= 35)	// 조건에 따라서 데이터를 걸르는것
+			.filter(user -> user.getName().contains("상원"))
 			.map(UserInfo::getName)	// 여러요소중 필요한 요소를 선택
 			.sorted()				// 데이터의 정렬
 			.forEach(s->System.out.println(s)); // 최종연산 하나씩 꺼내서 실행
